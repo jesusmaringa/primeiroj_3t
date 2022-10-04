75040a88f8ceecebb3fd413232210f89272cff60
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-04 15:41:23 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,7 @@
 // alert("Alerta: Alô mundo...");
 let tabuada = 4;
 
+
 function meChame(){
    document.write("Tabuado do " + tabuada + "<br>");
    document.write(tabuada + " x 1 = " + (tabuada*1) +"<br>");
@@ -14,4 +15,22 @@ function meChame(){
    document.write(tabuada + " x 8 = " + (tabuada*8) +"<br>");
    document.write(tabuada + " x 9 = " + (tabuada*9) +"<br>");
    document.write(tabuada + " x 10 = " + (tabuada*10) +"<br>");
+}
+
+
+function minhaTabuada(){
+   for(var i=1; i <= 10; i++){
+
+      for(var j = 1; j <=10; j++){
+         document.write(i + " x " + j + " = " + (i*j) +"<br>");
+      }
+
+      document.write("<br><br>");
+   }
+}
+
+function quadrado(){
+   for(var i = 2; i < 21; i++){
+      document.write("O quadrado de " + i + " é " + (i*i) + "<br>");
+   }
 }
